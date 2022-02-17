ad6b0d4121f2a6cb0f591cfc8e6c3b40721ad827
Nate Morrison <natem@nvsd.ca>
2022-02-17 11:53:39 -0800

    Replaced `strcpy` and `strcpy` with `sprintf` 	modified:   finder.c

--- finder.c
+++ finder.c
@@ -22,15 +22,11 @@ int main(int argc, char *argv[]) {
 	}
 	
 	if (argc == 2 && argv[2] == NULL) {
-		strcpy(cmd, "ls -R ");
-		strcat(cmd, argv[1]);
-		strcat(cmd, " | egrep -Hi \".png|.jpg|.jpeg|.gif|.jfif|.webp\" | wc -l");
+		sprintf(cmd, "ls -R %s | egrep -Hi \".png|.jpg|.jpeg|.gif|.jfif|.webp\" | wc -l", argv[1]);
 	}
 
 	if (strcmp(argv[1], "--full") == 0 || strcmp(argv[1], "-f") == 0) {
-		strcpy(cmd, "ls -R ");
-		strcat(cmd, argv[2]);
-		strcat(cmd, " | egrep -Hi \".jp2|.j2k|.jpf|.jpm|.jpg2|.j2c|.jpc|.jpx|.mj2|.jpg|.jpeg|.jpe|.jif|.jfif|.jfi|.png|.apng|.heif|.heifs|.heic|.heics|.avci|.avcs|.avif|.avifs|.3fr|.ari|.arw|.bay|.braw|.crw|.cr2|.cr3|.cap|.data|.dcs|.dcr|.dng|.drf|.eip|.erf|.fff|.gpr|.iiq|.k25|.kdc|.mdc|.mef|.mos|.mrw|.nef|.nrw|.obm|.orf|.pef|.ptx|.pxn|.r3d|.raf|.raw|.rwl|.rw2|.rwz|.sr2|.srf|.srw|.tif|.x3f|.tiff|.tif|.pbm|.pgm|.ppm|.pnm|.tga|.icb|.vda\" | wc -l"); // exotic formats
+		sprintf(cmd, "ls -R %s | egrep -Hi \".jp2|.j2k|.jpf|.jpm|.jpg2|.j2c|.jpc|.jpx|.mj2|.jpg|.jpeg|.jpe|.jif|.jfif|.jfi|.png|.apng|.heif|.heifs|.heic|.heics|.avci|.avcs|.avif|.avifs|.3fr|.ari|.arw|.bay|.braw|.crw|.cr2|.cr3|.cap|.data|.dcs|.dcr|.dng|.drf|.eip|.erf|.fff|.gpr|.iiq|.k25|.kdc|.mdc|.mef|.mos|.mrw|.nef|.nrw|.obm|.orf|.pef|.ptx|.pxn|.r3d|.raf|.raw|.rwl|.rw2|.rwz|.sr2|.srf|.srw|.tif|.x3f|.tiff|.tif|.pbm|.pgm|.ppm|.pnm|.tga|.icb|.vda\" | wc -l", argv[2]);
 	}
 
 	system(cmd);
